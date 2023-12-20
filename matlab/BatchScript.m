ca1f43dc32123ab8898780e40f77f5dd5f526201
Knowblesse <knowblesse@gmail.com>
2023-12-20 17:08:31 +0900

    better batch script

--- matlab/BatchScript.m
+++ matlab/BatchScript.m
@@ -11,27 +11,14 @@ filelist = dir(BASEPATH);
 sessionPaths = regexp({filelist.name},'(|#|##|@)AP\S*','match');
 sessionPaths = sessionPaths(~cellfun('isempty',sessionPaths));
 fprintf('%d sessions detected.\n', numel(sessionPaths));
-
+fprintf(strcat(repmat('=', 1, 80), '\n'));
 for session = 1 : numel(sessionPaths)
     tankName = cell2mat(sessionPaths{session});
     tankPath = fullfile(BASEPATH, tankName);
 
-    unitFilePaths = glob(tankPath, '\.(N|n)(T|t)(T|t)', true);
-    numCell = 0;
-    for unitFilePath = unitFilePaths
-        [Timestamps, ScNumbers, CellNumbers, Features, Samples] = Nlx2MatSpike(...
-        unitFilePath{1},...
-        [1, 1, 1, 1, 1],... % Time, Spike Channel Number, Cell Number, Spike Feature, Samples
-        0,... %Extract Header
-        1);
-    
-        numCell = numCell + numel(unique(CellNumbers));
-    end
-
-    % Show info
-    fprintf("================================================================================\n", tankName);
-    fprintf("Tank Name : %s\n", tankName);
-    fprintf("Num unit : %d\n", numCell);
+    createHelperFiles(tankPath);
     
     clearvars p_;
 end
+fprintf(strcat(repmat('=', 1, 80), '\n'));
+fprintf("BatchScript : All Complete! \n")
